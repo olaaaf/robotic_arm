6b5177c9e00412d8ab207ff1b5d901dcfc757af3
Olaf Login <o.login@outlook.com>
2023-05-24 14:33:18 +0200

    Rotation function with pivots working

--- Assets/Scripts/robot_rotation.cs
+++ Assets/Scripts/robot_rotation.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class robot_rotation : MonoBehaviour
 {
+    public GameObject[] arms =
+    {
+
+    };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       for (int i = 0; i < arms.Length; ++i){
+          Rotate(i, -0.3f);
+       }
+    }
+
+    public void Rotate(int arm_index, float alpha)
+    {
+      if (arm_index < 0 || arm_index > arms.Length)
+        return;
+      arms[arm_index].transform.Rotate(0, (arm_index > 0) ? 0 : alpha, (arm_index > 0) ? alpha : 0);
+
     }
 }
